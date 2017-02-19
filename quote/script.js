6b9fce87835631be67e8fe238c21d63f83e5083f
junkdeck <fred@mslsr.com>
2017-02-19 21:10:13 +0100

    http
    god damnit

--- quote/script.js
+++ quote/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('.getter').on('click',function(){
-        $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?", function(data){
+        $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?", function(data){
             $(".quote").html(data.quoteText);
             $(".author").html(data.quoteAuthor);
         });
